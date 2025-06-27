8004e02f7a86b82096af8b93cbbdd3728bd38075
Dennis30 <72525447@certus.edu.pe>
2025-06-27 01:14:00 -0500

    Avance76

--- js/navbar.js
+++ js/navbar.js
@@ -6,13 +6,12 @@ const navbarHTML = `
   <div class="nav">
     <ul>
       <li><a class="boton-navegacion" href="/Adopet//index.html">INICIO</a></li>
-      <li><a class="boton-navegacion" href="/Adopet/tienda.html">TIENDA</a></li>
+      <li><a class="boton-navegacion" href="../html/tienda.html">TIENDA</a></li>
       <li><a class="boton-navegacion" href="">SERVICIOS</a></li>
       <li><a class="boton-navegacion" href="">ADOPCIONES</a></li>
       <li><a class="boton-navegacion" href="">CONTACTO</a></li>
     </ul>
-    <p><a class="boton-registrar" href="/Adopet/regristro.html">REGISTRATE</a></p>
-  </div>
+    <p><a class="boton-registrar" href="../html/regristro.html">REGISTRATE</a></p>  </div>
 </nav>
 `;
 
