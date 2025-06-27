952aa5ecdba900602a0efe60a473d5b83cfd0ff0
Dennis30 <72525447@certus.edu.pe>
2025-06-27 00:57:26 -0500

    Avance56

--- js/navbar.js
+++ js/navbar.js
@@ -1,17 +1,17 @@
 const navbarHTML = `
 <nav>
-  <a class="adopet" href="/index.html">Adopet</a>
+  <a class="adopet" href="/Adopet//index.html">Adopet</a>
   <input type="checkbox" id="menu-toggle" class="menu-toggle" hidden>
   <label for="menu-toggle" class="hamburguesa">&#9776;</label>
   <div class="nav">
     <ul>
-      <li><a class="boton-navegacion" href="/index.html">INICIO</a></li>
-      <li><a class="boton-navegacion" href="/tienda.html">TIENDA</a></li>
+      <li><a class="boton-navegacion" href="/Adopet//index.html">INICIO</a></li>
+      <li><a class="boton-navegacion" href="/Adopet/tienda.html">TIENDA</a></li>
       <li><a class="boton-navegacion" href="">SERVICIOS</a></li>
       <li><a class="boton-navegacion" href="">ADOPCIONES</a></li>
       <li><a class="boton-navegacion" href="">CONTACTO</a></li>
     </ul>
-    <p><a class="boton-registrar" href="/regristro.html">REGISTRATE</a></p>
+    <p><a class="boton-registrar" href="/Adopet//regristro.html">REGISTRATE</a></p>
   </div>
 </nav>
 `;
