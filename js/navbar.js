f99fe1f7c10ebee30e5daab44bc15897ad3c7918
Dennis30 <72525447@certus.edu.pe>
2025-06-27 00:29:15 -0500

    Avance34

--- js/navbar.js
+++ js/navbar.js
@@ -6,7 +6,7 @@ const navbarHTML = `
   <div class="nav">
     <ul>
       <li><a class="boton-navegacion" href="../index.html">INICIO</a></li>
-      <li><a class="boton-navegacion" href="../Adopet/html/tienda.html">TIENDA</a></li>
+      <li><a class="boton-navegacion" href="../html/tienda.html">TIENDA</a></li>
       <li><a class="boton-navegacion" href="">SERVICIOS</a></li>
       <li><a class="boton-navegacion" href="">ADOPCIONES</a></li>
       <li><a class="boton-navegacion" href="">CONTACTO</a></li>
